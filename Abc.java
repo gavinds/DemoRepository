d88061bc14a26200a33f29cf25a4e56697fd1d55
gavinds <gavindsouza09@gmail.com>
2021-03-24 17:13:37 +0530

    Created Abc.java and Created Xyz.java

--- Abc.java
+++ Abc.java
@@ -2,6 +2,7 @@ public class Abc
 {
 	public static void main(String args[])
 	{
+		System.out.println("this is new");
 		System.out.println("hi");
 	}
 }
---
+++ Xyz.java
@@ -0,0 +1,12 @@
+public class Xyz
+{
+	private int x;
+	public void setx(int x)
+	{
+		this.x=x;
+	}
+	pulic int getx()
+	{
+		return x;
+	}
+}
